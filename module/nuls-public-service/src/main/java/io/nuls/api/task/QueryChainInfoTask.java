e29f70ce197acbe7a5aa943774c10ff7d28c6f00
vivi <251556768@qq.com>
2019-12-10 15:22:28 +0800

    update get asset config

--- module/nuls-public-service/src/main/java/io/nuls/api/task/QueryChainInfoTask.java
+++ module/nuls-public-service/src/main/java/io/nuls/api/task/QueryChainInfoTask.java
@@ -17,36 +17,49 @@ public class QueryChainInfoTask implements Runnable {
 
     private int chainId;
 
-
     public QueryChainInfoTask(int chainId) {
         this.chainId = chainId;
     }
 
     @Override
     public void run() {
-        Map<Integer, ChainInfo> chainInfoMap;
-        Map<String, AssetInfo> assetInfoMap;
         try {
+            Map<Integer, ChainInfo> chainInfoMap = new HashMap<>();
+            Map<String, AssetInfo> assetInfoMap = new HashMap<>();
+            //添加本链默认链信息和资产信息
+            ApiCache apiCache = CacheManager.getCache(chainId);
+            ChainInfo chainInfo = apiCache.getChainInfo();
+            chainInfoMap.put(chainInfo.getChainId(), chainInfo);
+            assetInfoMap.put(chainInfo.getDefaultAsset().getKey(), chainInfo.getDefaultAsset());
+
+
+            //如果是跨链 ，还需要添加跨链资产
             if (ApiContext.isRunCrossChain) {
                 Result<Map<String, Object>> result = WalletRpcHandler.getRegisteredChainInfoList();
                 Map<String, Object> map = result.getData();
-                CacheManager.setChainInfoMap((Map<Integer, ChainInfo>) map.get("chainInfoMap"));
-                CacheManager.setAssetInfoMap((Map<String, AssetInfo>) map.get("assetInfoMap"));
-                ApiContext.isReady = true;
-            } else {
-                chainInfoMap = new HashMap<>();
-                assetInfoMap = new HashMap<>();
-                ApiCache apiCache = CacheManager.getCache(chainId);
-                ChainInfo chainInfo = apiCache.getChainInfo();
-                chainInfoMap.put(chainInfo.getChainId(), chainInfo);
-                assetInfoMap.put(chainInfo.getDefaultAsset().getKey(), chainInfo.getDefaultAsset());
+                Map<Integer, ChainInfo> chainInfoMap1 = (Map<Integer, ChainInfo>) map.get("chainInfoMap");
+                for (ChainInfo chainInfo1 : chainInfoMap1.values()) {
+                    if (!chainInfoMap.containsKey(chainInfo1.getChainId())) {
+                        chainInfoMap.put(chainInfo1.getChainId(), chainInfo1);
+                    }
+                }
 
+                Map<String, AssetInfo> assetInfoMap1 = (Map<String, AssetInfo>) map.get("assetInfoMap");
+                for (AssetInfo assetInfo : assetInfoMap1.values()) {
+                    if (!assetInfoMap.containsKey(assetInfo.getKey())) {
+                        assetInfoMap.put(assetInfo.getKey(), assetInfo);
+                    }
+                }
                 CacheManager.setChainInfoMap(chainInfoMap);
                 CacheManager.setAssetInfoMap(assetInfoMap);
-
+                ApiContext.isReady = true;
+            } else {
+                CacheManager.setChainInfoMap(chainInfoMap);
+                CacheManager.setAssetInfoMap(assetInfoMap);
                 ApiContext.isReady = true;
             }
-        }catch (Exception e) {
+
+        } catch (Exception e) {
             LoggerUtil.commonLog.error(e);
         }
     }
